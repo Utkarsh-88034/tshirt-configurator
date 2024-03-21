35c93922e5836fffd63eceaaf41f5b546e72b445
Utkarsh-88034 <robbin7838@gmail.com>
2024-03-21 22:49:12 +0530

    basic rotation is applied

--- src/RenderImage.jsx
+++ src/RenderImage.jsx
@@ -8,30 +8,33 @@ const RenderImage = ({ position }) => {
   const { camera, gl } = useThree();
   const imgRef = useRef();
   const scaleTexture = useTexture("/scale.svg");
-  const [showScale, setShowScale] = useState([false, new THREE.Vector3()]);
+  const rotateTexture = useTexture("/rotate.svg");
+
+  const [showGizmo, setShowGizmo] = useState([false, null]);
   const [isDragging, setIsDragging] = useState(false);
+  const [gizmoType, setGizmoType] = useState("scale");
   const initialMousePosition = useRef(null);
   const initialScale = useRef(null);
+  const initialRotation = useRef(null);
+
   const dragRef = useRef(null);
   const handleImageHover = (img) => {
     img.geometry.computeBoundingBox();
     const boundingBox = img.geometry.boundingBox;
 
-    setShowScale([
-      true,
-      showScale[1].set(boundingBox.max.x, boundingBox.min.y, 0),
-    ]);
+    setShowGizmo([true, boundingBox]);
   };
 
-  const handlePointerDown = (event) => {
-    console.log(dragRef);
-    // dragRef.current.enabled = false;
+  const handlePointerDown = (event, type) => {
     setIsDragging(true);
+    setGizmoType(type);
     initialMousePosition.current = new THREE.Vector2(
       event.clientX,
       event.clientY
     );
     initialScale.current = imgRef.current.scale.clone();
+    initialRotation.current = imgRef.current.rotation.clone();
+    console.log(initialRotation);
   };
 
   const handlePointerUp = () => {
@@ -43,17 +46,31 @@ const RenderImage = ({ position }) => {
   useEffect(() => {
     const handlePointerMove = (event) => {
       if (isDragging && event.buttons == 1) {
+        dragRef.current.deactivate();
+
         const mouseDelta = new THREE.Vector2(event.clientX, event.clientY).sub(
           initialMousePosition.current
         );
-        const scaleDelta = mouseDelta.x * 0.01;
-        imgRef.current.scale.x = initialScale.current.x + scaleDelta;
-        imgRef.current.scale.y = initialScale.current.y + scaleDelta;
+        if (gizmoType == "scale") {
+          const scaleDelta = mouseDelta.x * 0.01;
+          imgRef.current.scale.x = initialScale.current.x + scaleDelta;
+          imgRef.current.scale.y = initialScale.current.y + scaleDelta;
+        } else if (gizmoType == "rotate") {
+          const rotateDeltaX = mouseDelta.x * 0.01;
+          const rotateDeltaY = mouseDelta.y * 0.01;
+
+          // Adjust rotation around the Z-axis based on both X and Y mouse deltas
+          imgRef.current.rotation.z =
+            initialRotation.current.z + rotateDeltaX - rotateDeltaY;
+
+          //   imgRef.current.rotation.y = initialRotation.current.y + rotateDelta;
+        }
       }
     };
 
     const handleMouseUp = () => {
       setIsDragging(false);
+      dragRef.current.activate();
     };
 
     window.addEventListener("mousemove", handlePointerMove);
@@ -66,8 +83,14 @@ const RenderImage = ({ position }) => {
   }, [isDragging]);
 
   useEffect(() => {
-    const controls = new DragControls([imgRef.current], camera, gl.domElement);
-    controls.transfromGroup = true;
+    if (!dragRef.current && imgRef.current) {
+      dragRef.current = new DragControls(
+        [imgRef.current],
+        camera,
+        gl.domElement
+      );
+      dragRef.current.transfromGroup = true;
+    }
   }, [imgRef.current]);
   return (
     <Image
@@ -80,19 +103,31 @@ const RenderImage = ({ position }) => {
         handleImageHover(e.eventObject);
       }}
       onPointerLeave={() => {
-        setShowScale([false, showScale[1]]);
+        setShowGizmo([false, showGizmo[1]]);
       }}
     >
-      {showScale[0] && (
+      {showGizmo[0] && (
         <>
           <Edges linewidth={10} scale={1} threshold={15} color="red" />
           <mesh
-            position={showScale[1]}
-            onPointerDown={handlePointerDown}
+            position={[showGizmo[1].max.x, showGizmo[1].min.y, 0]}
+            onPointerDown={(e) => {
+              handlePointerDown(e, "scale");
+            }}
             onPointerUp={handlePointerUp}
           >
             <planeGeometry args={[0.2, 0.2]} />
             <meshPhysicalMaterial map={scaleTexture} transparent />
+          </mesh>
+          <mesh
+            position={[showGizmo[1].max.x, showGizmo[1].max.y, 0]}
+            onPointerDown={(e) => {
+              handlePointerDown(e, "rotate");
+            }}
+            onPointerUp={handlePointerUp}
+          >
+            <planeGeometry args={[0.2, 0.2]} />
+            <meshPhysicalMaterial map={rotateTexture} transparent />
           </mesh>
         </>
       )}
