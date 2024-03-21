4d2465ddc09082bda3f34aa0bd5afd584aa3863e
Utkarsh-88034 <robbin7838@gmail.com>
2024-03-21 16:25:03 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
-import { fabric } from "fabric";
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+import { Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stage } from "@react-three/drei";
-import { TEE } from "./Tee";
-import { CanvasTexture } from "three";
-import * as THREE from "three";
-import { Tee2 } from "./Tee2";
-import { TEE3 } from "./Tee3";
-import { TEE4 } from "./Tee4";
-import { Tee6 } from "./Tee6";
-import { Tee8 } from "./Tee8";
-import { Shirt } from "./Shirt";
-import { Shirt4 } from "./Shirt4";
+import "./App.css";
 import RenderFront from "./RenderFront";
 
 function App() {
